0f772bb3c10264a14ef5aaccd39ff267f3c5ef82
Prayag Tandon <tandonprayag99@gmail.com>
2023-05-18 13:23:56 +0530

    JS half game completed

--- JS/script.js
+++ JS/script.js
@@ -1,25 +1,53 @@
 'use strict';
 
-// const newHeadingTertiary = document.querySelector('.heading-tertiary').textContent = 'Checking your score...';
+// GENERATION OF A RANDOM NUMBER B/W 1 and 20...
 
-// console.log(newHeadingTertiary);
+const secretNum = Math.floor(Math.random() * 20) + 1;
+console.log(secretNum);
+let score = 20;
+let highScore = 0;
 
-// console.log(document.getElementsByClassName('question-symbol'));
-// console.log(document.getElementsByClassName('btn'));
+const checkBtn = document.querySelector('.btn--check');
 
-// console.log(document.getElementsByTagName('h2'));
-// console.log(document.getElementsByTagName('*'));
+checkBtn.addEventListener('click', function () {
+    const guessInput = Number(document.querySelector('.guess').value);
+    // console.log(guessInput, typeof guessInput);
 
-// console.log(document.querySelectorAll('.question-symbol')[1].textContent);
-// console.log(document.querySelector('.question-symbol').textContent);
+    if (guessInput > 0) {
 
-// let checkBtn = document.querySelector('.btn--check');
+        if (guessInput === secretNum) {
+            document.querySelector('.heading-tertiary').textContent = '🎉🎉 Correct Number';
+            const symbolQues = document.querySelectorAll('.symbol-span');
+            for (let i = 0; i < symbolQues.length; i++) {
+                symbolQues[i].textContent = secretNum;
+            }
+            if (score > highScore) {
+                highScore = score;
+                document.querySelector('.highscore').textContent = highScore;
+            }
+            checkBtn.disabled = true;
+            checkBtn.classList.add('btn--invalid');
+        }
 
-// checkBtn.addEventListener('click', function () {
-//     let symbolQues = document.querySelectorAll('.symbol-span');
-//     console.log(symbolQues);
-//     for (let i = 0; i < symbolQues.length; i++) {
-//         symbolQues[i].textContent = 5;
-//     }
-//     console.log(typeof symbolQues);
-// })
+        else if (guessInput > secretNum) {
+            if (score > 1) {
+                document.querySelector('.heading-tertiary').textContent = '📈📈 Too high!!';
+                score--;
+                document.querySelector('.score').textContent = score;
+            }
+        }
+
+        else if (guessInput < secretNum) {
+            if (score > 1) {
+                document.querySelector('.heading-tertiary').textContent = '📉📉 Too Low!!';
+                score--;
+                document.querySelector('.score').textContent = score;
+            }
+        }
+    }
+    else if (guessInput === 0 || guessInput < 0) {
+        const redHeading = document.querySelector('.heading-tertiary');
+        redHeading.textContent = '⛔⛔ Not a valid number';
+        redHeading.style.color = '#c62a02';
+    }
+})
