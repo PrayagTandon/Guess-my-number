20140331d4e6b0ad49b2321423e65e92342ebebf
Prayag Tandon <tandonprayag99@gmail.com>
2023-05-18 10:43:03 +0530

    Minor addition and changes

--- JS/script.js
+++ JS/script.js
@@ -3,3 +3,23 @@
 // const newHeadingTertiary = document.querySelector('.heading-tertiary').textContent = 'Checking your score...';
 
 // console.log(newHeadingTertiary);
+
+// console.log(document.getElementsByClassName('question-symbol'));
+// console.log(document.getElementsByClassName('btn'));
+
+// console.log(document.getElementsByTagName('h2'));
+// console.log(document.getElementsByTagName('*'));
+
+// console.log(document.querySelectorAll('.question-symbol')[1].textContent);
+// console.log(document.querySelector('.question-symbol').textContent);
+
+// let checkBtn = document.querySelector('.btn--check');
+
+// checkBtn.addEventListener('click', function () {
+//     let symbolQues = document.querySelectorAll('.symbol-span');
+//     console.log(symbolQues);
+//     for (let i = 0; i < symbolQues.length; i++) {
+//         symbolQues[i].textContent = 5;
+//     }
+//     console.log(typeof symbolQues);
+// })
