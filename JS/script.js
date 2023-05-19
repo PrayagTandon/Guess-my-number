fc60922a2bdc86d4c7133567350efc22df84bef2
Prayag Tandon <tandonprayag99@gmail.com>
2023-05-19 10:51:28 +0530

    New addition to JS

--- JS/script.js
+++ JS/script.js
@@ -2,18 +2,20 @@
 
 // GENERATION OF A RANDOM NUMBER B/W 1 and 20...
 
-const secretNum = Math.floor(Math.random() * 20) + 1;
+let secretNum = Math.floor(Math.random() * 20) + 1;
 console.log(secretNum);
 let score = 20;
 let highScore = 0;
+
+const gameDescription = document.querySelector('.game--description');
+const gameContent = document.querySelector('.game--content');
 
 const checkBtn = document.querySelector('.btn--check');
 
 checkBtn.addEventListener('click', function () {
     const guessInput = Number(document.querySelector('.guess').value);
-    // console.log(guessInput, typeof guessInput);
 
-    if (guessInput > 0) {
+    if (guessInput > 0 && guessInput <= 20) {
 
         if (guessInput === secretNum) {
             document.querySelector('.heading-tertiary').textContent = '🎉🎉 Correct Number';
@@ -27,6 +29,10 @@ checkBtn.addEventListener('click', function () {
             }
             checkBtn.disabled = true;
             checkBtn.classList.add('btn--invalid');
+
+            gameDescription.classList.add('game--won');
+            gameContent.classList.add('game--won');
+
         }
 
         else if (guessInput > secretNum) {
@@ -45,9 +51,30 @@ checkBtn.addEventListener('click', function () {
             }
         }
     }
-    else if (guessInput === 0 || guessInput < 0) {
+    else if (guessInput <= 0 || guessInput > 20) {
         const redHeading = document.querySelector('.heading-tertiary');
         redHeading.textContent = '⛔⛔ Not a valid number';
         redHeading.style.color = '#c62a02';
+    }
+})
+
+const btnReset = document.querySelector('.btn--new');
+
+btnReset.addEventListener('click', function () {
+
+    secretNum = Math.floor(Math.random() * 20) + 1;
+    console.log(secretNum);
+    score = 20;
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.heading-tertiary').textContent = 'Start guessing...';
+    gameDescription.classList.remove('game--won');
+    gameContent.classList.remove('game--won');
+    checkBtn.disabled = false;
+    checkBtn.classList.remove('btn--invalid');
+    document.querySelector('.guess').value = '';
+
+    const symbolQues = document.querySelectorAll('.symbol-span');
+    for (let i = 0; i < symbolQues.length; i++) {
+        symbolQues[i].textContent = '?';
     }
 })
